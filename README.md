2eb76a333e2146946480600deab79e0170519c36
Sherry Freitas <sherry.freitas@gmail.com>
2019-05-30 13:36:38 -0700

    adds initial project files

---
+++ README.md
@@ -0,0 +1,21 @@
+# NODE101 Hello HTTP World
+
+To install:
+```
+npm install
+```
+---
+To test, start a server in one terminal:
+```
+npm start
+```
+Then, run in another terminal:
+```
+npm test
+```
+---
+To run, 
+```
+npm start
+```
+Then, in your browser, visit http:\127.0.0.1:8080
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code here
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+    console.log(`Received ${req.method} request for:${req.url}`);
+});
+
+server.listen(8080,'127.0.0.1', () => {
+    console.log(`Server is listenting at http:\\127.0.0.1:8080`);
+});
